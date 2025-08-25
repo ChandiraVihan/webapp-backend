9f97182d5a2defbd45f9db6ca7f0c12f09198d34
ChandiraVihan <chandiravihan@gmail.com>
2025-08-25 19:04:55 +0530

    Update ProductController.java

--- src/main/java/com/loginproject/loginapp/ProductController.java
+++ src/main/java/com/loginproject/loginapp/ProductController.java
@@ -12,33 +12,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController; // --- IMPORT THIS ---
+import org.springframework.web.bind.annotation.RestController; 
 
 @RestController
 @RequestMapping("/api/products")
-// --- THIS IS THE LINE TO CHANGE ---
 @CrossOrigin(
-    origins = "http://localhost:3000", 
-    methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE} // Explicitly allow DELETE
+    origins = "http://localhost:3000",
+    methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.DELETE} 
 )
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepo;
 
-    // GET all products
+    
     @GetMapping
     public List<Product> getAllProducts() {
         return productRepo.findAll();
     }
 
-    // POST a new product
     @PostMapping
     public Product addProduct(@RequestBody Product product) {
         return productRepo.save(product);
     }
     
-    // DELETE a product
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteProduct(@PathVariable Long id) {
         if (!productRepo.existsById(id)) {
