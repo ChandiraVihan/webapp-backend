037f1d08bda1d5ed209e97a6178ac9957f34ff35
Minxpat <it24101496@my.sliit.lk>
2025-08-27 20:38:54 +0530

    Added ProductRepository

--- src/main/java/com/loginproject/loginapp/ProductRepository.java
+++ src/main/java/com/loginproject/loginapp/ProductRepository.java
@@ -1,7 +1,23 @@
 package com.loginproject.loginapp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-   
+
+    // Find products by category
+    List<Product> findByCategory(String category);
+
+    // Find products by name (case-insensitive search)
+    List<Product> findByProNameContainingIgnoreCase(String pro_name);
+
+    // Find products cheaper than or equal to a given price
+    List<Product> findByPriceLessThanEqual(double price);
+
+    // Find products more expensive than or equal to a given price
+    List<Product> findByPriceGreaterThanEqual(double price);
+
+    //Find products in a price range
+    List<Product> findByPriceBetween(double minPrice, double maxPrice);
 }
+
